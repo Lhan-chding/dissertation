92480b741a6b9cc1ee77195ed2261de07ed404d2
Lhan-chding <1223128329@qq.com>
2026-08-22 19:53:12 +0800

    chore: lint study-c2 entrypoints

--- scripts/v5/study_c2/20_audit_legacy_study_c_parser.py
+++ scripts/v5/study_c2/20_audit_legacy_study_c_parser.py
@@ -2,6 +2,6 @@ from _bootstrap import bootstrap_repo
 
 bootstrap_repo()
 
-from compensability_v5.study_c2.cli import run_registered
+from compensability_v5.study_c2.cli import run_registered  # noqa: E402
 
 raise SystemExit(run_registered(20))
